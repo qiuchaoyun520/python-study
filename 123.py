bca8f5bdb8932c14f8200ed0d4ebe11843394108
coder-pig <2927932190@qq.com>
2021-07-29 16:57:37 +0800

    123

--- 123.py
+++ 123.py
@@ -1,1 +1,18 @@
-scdzvdvxzcvx
+# -*- coding:utf-8 -*-
+import cv2
+import numpy as np
+
+# 读取图片
+img = cv2.imread("C:\\1.jpg", cv2.IMREAD_COLOR)
+
+# 拆分通道
+b, g, r = cv2.split(img)
+
+# 显示原始图像
+cv2.imshow("B", b)
+cv2.imshow("G", g)
+cv2.imshow("R", r)
+
+# 等待显示
+cv2.waitKey(0)
+cv2.destroyAllWindows()
